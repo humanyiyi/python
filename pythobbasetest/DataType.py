b6ad7d9314a80dbd6ac2fc480c3a01847dcaf8ab
lenovo <2672889064@qq.com>
2018-08-29 23:25:20 +0800

    python基础

--- pythobbasetest/DataType.py
+++ pythobbasetest/DataType.py
@@ -40,3 +40,47 @@ print(float_num3) #4.0
 #复数 整数+虚数 ： 1 + 2j
 complex_num = 1 + 2j
 print(complex_num)  #(1+2j)
+
+#2.String 字符串
+#字符串表达形式，
+# 单引号，双引号： 表示一行 ，
+#三单引号，三双引号： 可以表示多行，允许换行
+
+str1 = 'hello world'
+print(str1)  #hello world
+str2 = "hello World"
+print(str2) #hello World
+#str3 = 'hello world"  #单引号和双引号要成对出现，否则报错
+
+#单引号和双引号在字符串表示上没有什么区别，不过当字符串中有引号时可以区分
+str4 = 'you are my "angle"'
+print(str4) #you are my "angle"
+
+#单引号和双引号并不能表示换行，如果有换行操作，可是使用 ''' ''' 或者 """ """
+
+'''
+str5的输出：
+you 
+and
+me
+'''
+str5 = '''
+you 
+and
+me
+'''
+print(str5)
+
+''' str6的输出：
+you are my 
+ angel;
+H LOVE Y
+'''
+str6 = """
+you are my \n angel;
+H LOVE Y
+"""
+print(str6)
+
+
+#3.List 列表
