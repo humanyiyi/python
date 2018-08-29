86e32001456e0786be2b067a53bb6e21970a06f1
lenovo <2672889064@qq.com>
2018-08-29 23:12:12 +0800

    python基础

--- pythobbasetest/DataType.py
+++ pythobbasetest/DataType.py
@@ -13,18 +13,30 @@ python3中有6种标准数据类型
 #Number
 #整数
 int_num = 18
-print(int_num)
+print(int_num)  #18
 
 #定义变量
 num1,num2,num3 = 11,31,22
-print(num1)
-print(num2);
-print(num3)
+print(num1) #11
+print(num2); #31
+print(num3) #22
+
+#数值计算
+print(num1+num3) #33
+print(num3-num2)   # -9
+print(num1*num2)  #341
+print(num2/num1) #2.8181818181818183
+print(num2%num1) #9
+print(num3/num1)  #2.0
 
 #浮点型
 float_num1 = 1.2
 float_num2 = .5
 float_num3 = 4.
-print(float_num1)
-print(float_num2)
-print(float_num3)
+print(float_num1) #1.2
+print(float_num2) #0.5
+print(float_num3) #4.0
+
+#复数 整数+虚数 ： 1 + 2j
+complex_num = 1 + 2j
+print(complex_num)  #(1+2j)
