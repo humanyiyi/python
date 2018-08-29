d1ec793901a9476c39e34b8e332da808c33482d7
lenovo <2672889064@qq.com>
2018-08-29 22:27:26 +0800

    python基础

---
+++ pythobbasetest/DataType.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+'''
+python3中有6种标准数据类型
+1.Number 数字
+2.String 字符串
+3.List 列表
+4.Tuple 元组
+5.Set 集合
+6.Dictionary 字典
+'''
+
+#Number
+#整数
+int_num = 18
+print(int_num)
+
+#定义变量
+num1,num2,num3 = 11,31,22
+print(num1)
+print(num2);
+print(num3)
+
+#浮点型
+float_num1 = 1.2
+float_num2 = .5
+float_num3 = 4.
+print(float_num1)
+print(float_num2)
+print(float_num3)
---
+++ pythobbasetest/HelloWorld.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+#python入门：答应Hello World
+print('Hello world')
+
+
+#python函数 def
+def hello_world() :
+    print('hello world')
+
+hello_world()
